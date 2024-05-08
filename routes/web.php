9c1aa246d95d08b08988ce0257a40d9819bd3e37
take1616 <takenoko11061106@gmail.com>
2024-05-08 11:53:40 +0900

    カリキュラム8-3

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,8 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;      
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [PostController::class, 'index']);
 
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
-
-
-Route::get('/posts', [PostController::class, 'index']);   
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/posts/{post}', [PostController::class ,'show']);
+// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
